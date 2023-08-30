b069cec28c1664cf177c6ad50e6a17677d7c892f
Michael Chen <mikaloramik@gmail.com>
2023-08-30 12:55:41 -0700

    add try catch to gif-creator example

--- gif-creator/src/index.js
+++ gif-creator/src/index.js
@@ -3,25 +3,37 @@ import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 
 // Handler
-export async function handler(event, context) {
+export const handler =  async function (event, context) {
+  console.log('Received event');
   const pluginInvocationToken = event.pluginInvocationToken;
   const callbackUrl = event.callbackUrl;
-
-  const assetUrls = event.assets.map((asset) => asset.url);
-  const gifBuffer = await createGif(assetUrls);
+  try {
 
   const callbackResponse = await createAsset(
     callbackUrl,
     pluginInvocationToken,
     uuidv4() + ".gif"
   );
+  console.log('created skeleton asset');
 
   const uploadUrl = callbackResponse.data.assets[0].uploadUrl;
 
+  const assetUrls = event.assets.map((asset) => asset.url);
+  const gifBuffer = await createGif(assetUrls);
+  console.log('created gif');
+
   await uploadImage(uploadUrl, gifBuffer);
-  reportStatus(callbackUrl, pluginInvocationToken, "success");
+  console.log('uploaded gif');
+
+  await reportStatus(callbackUrl, pluginInvocationToken, "success");
+  console.log('reported success');
 
   return;
+  } catch (error) {
+    console.log('error: ', error);
+    await reportStatus(callbackUrl, pluginInvocationToken, "failed");
+    console.log('reported failure');
+  }
 }
 
 async function createAsset(callbackUrl, pluginInvocationToken, title) {

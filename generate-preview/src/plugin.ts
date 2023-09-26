979426120efdd2e243d1bf67952928ff80d77b25
Michael Chen <mikaloramik@gmail.com>
2023-09-25 21:18:26 -0700

    add generate-preview example (#13)
    * add generate-preview example
    
    * remove dist
    
    * account for new API
    
    * renames

---
+++ generate-preview/src/plugin.ts
@@ -0,0 +1,30 @@
+import {
+  PlaybookAPI,
+  uploadBufferToUrl,
+} from "./lib";
+
+import axios from "axios";
+
+export default async function ({
+  pluginInvocationToken,
+  callbackUrl,
+  assets,
+}: PluginInvocationParams) {
+  console.log("Invoked plugin", callbackUrl);
+
+  const playbookAPI = new PlaybookAPI({
+    pluginInvocationToken,
+    callbackUrl,
+  });
+
+  const inputAsset = assets[0];
+
+  const inputBuffer = (await axios({ url: "https://img.playbook.com/BZ5cWTaMqd9tP1aTWKWr2Z8I1SI-lx6hI7U0p2EpBr0/Z3M6Ly9wbGF5Ym9v/ay1hc3NldHMtcHVi/bGljLzE4MWI4YzZi/LTU1ZDctNDExZi1i/YTcyLTE4MTk5Mzkw/NGRlOA", responseType: "arraybuffer" })).data as Buffer;
+
+  const response = await playbookAPI.generateAssetPreviewUrl(inputAsset.token, 1201, 1201);
+
+  await uploadBufferToUrl(inputBuffer, response.data.newPreviews[0].previewUploadUrl)
+  await playbookAPI.reportStatus("success");
+
+  console.log("Done");
+}
